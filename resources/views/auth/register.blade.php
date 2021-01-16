82a705724e6f38f17fd38132b8b5875d45b4c431
katheesh <katheeskumar@outlook.com>
2021-01-16 21:38:28 +0530

    logo added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+           <img src="https://gitlab.com/katheesh/public/-/raw/master/clipup%20icon/ClipUPIcoW-01.png" width="100">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
